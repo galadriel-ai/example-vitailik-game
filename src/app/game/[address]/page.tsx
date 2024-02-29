e133ab4b21eb1c4fa6aa4d2ed70aca4c612f80f6
taivop <taivo@pungas.ee>
2024-02-29 16:05:36 -0700

    rm unused

--- src/app/game/[address]/page.tsx
+++ src/app/game/[address]/page.tsx
@@ -29,26 +29,7 @@ const {networkConfig} = createNetworkConfig({
   },
 });
 
-
-const SUI_ADDRESS_LENGTH: number = 32;
-
-const isHex = (value: string): boolean => {
-  return /^(0x|0X)?[a-fA-F0-9]+$/.test(value) && value.length % 2 === 0;
-}
-
-const getHexByteLength = (value: string): number => {
-  return /^(0x|0X)/.test(value) ? (value.length - 2) / 2 : value.length / 2;
-}
-
-const isValidSuiAddress = (value: string): boolean => {
-  return isHex(value) && getHexByteLength(value) === SUI_ADDRESS_LENGTH;
-}
-
 const queryClient = new QueryClient()
-
-const onSwitchNetwork = (network: Network) => {
-   // TODO Kristjan pls help all I am just copy pasting things with chatgpt
-}
 
 
 export default function Page({
