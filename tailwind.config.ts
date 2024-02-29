1ee4997369e0ca1d2c0e6dabb0bd0f0473aab38e
Taivo Pungas <taivo@pungas.ee>
2024-02-29 12:58:00 -0700

    tp/design (#1)
    * add custom font
    
    * font and color fixes
    
    * last color fixes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,16 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    fontFamily: {
+      PPMondwest: ['PPMondwest-Regular', 'sans-serif'],
+    },
+    colors: {
+      brand: {
+        blue: "#0657e0",
+        bluedark: "#002360",
+        neongreen: "#00ff66"
+      }
+    }
   },
   plugins: [
     require("@tailwindcss/forms"),
