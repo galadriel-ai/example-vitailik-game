c8970e62d86d98dc336031dafd01d264e44643c3
taivop <taivo@pungas.ee>
2024-03-01 12:37:30 -0700

    progressbar restarts

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
+const FUN_WAITING_MESSAGES = ["on chain ai is slow lol", "kek", "lel", "pip"];
+
 interface ProgressBarProps {
   duration: number; // Total duration of progress in seconds
   message: string;
@@ -7,29 +9,35 @@ interface ProgressBarProps {
 
 export const ProgressBar: React.FC<ProgressBarProps> = ({ duration, message }) => {
   const [progress, setProgress] = useState(1);
+  const [randomMessage, setRandomMessage] = useState("");
 
   useEffect(() => {
-    // Calculate the interval time to update the progress state
-    const intervalTime = (duration * 1000) / 100;
+    setRandomMessage(
+      FUN_WAITING_MESSAGES[Math.floor(Math.random() * FUN_WAITING_MESSAGES.length)]
+    );
 
+    const intervalTime = (duration * 1000) / 100;
     const interval = setInterval(() => {
       setProgress((prevProgress) => {
         const nextProgress = prevProgress + 1;
         if (nextProgress > 100) {
-          clearInterval(interval);
-          return 100;
+          //clearInterval(interval);
+          //return 100;
+          return 0;
         }
         return nextProgress;
       });
     }, intervalTime);
 
-    // Clear the interval on component unmount
     return () => clearInterval(interval);
   }, [duration]);
 
   return (
     <>
-      <p className='mb-2'>{message}</p>
+      <p className='mb-2'>
+        <span className="">{message}&nbsp;</span>
+        <span className="text-brand-neongreen">{randomMessage}</span>
+      </p>
       <div className="w-full bg-white h-2.5 dark:bg-gray-700">
         <div
           className="bg-brand-neongreen h-2.5"
