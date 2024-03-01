2d2fd0e1d55e54786914cbf5e264e0e1e48d28c5
kristjanpeterson <kristjan@nftport.xyz>
2024-03-01 18:21:12 +0200

    Walletconnect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,31 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import "./globals.css";
+import {ContextProvider} from "@/context";
+import {cookieToInitialState} from "wagmi";
+import {config} from "@/config";
+import {headers} from "next/headers";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "VitAIlik Fight Club",
   description: "Battle with on-chain AI 'VitAIlik'",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout(
+  {
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>
+) {
+  const initialState = cookieToInitialState(config, headers().get('cookie'))
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <body className={inter.className}>
+    <ContextProvider initialState={initialState}>{children}</ContextProvider>
+    </body>
     </html>
   );
 }
