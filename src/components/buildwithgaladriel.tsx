7c4540b746874a0095eef960e3fa2e6b326f9f76
taivop <taivo@pungas.ee>
2024-03-01 10:31:03 -0700

    build with gal font size

--- src/components/buildwithgaladriel.tsx
+++ src/components/buildwithgaladriel.tsx
@@ -1,6 +1,6 @@
 export const BuildWithGaladriel = () => (
     <>
-        <div className="fixed bottom-0 right-0 p-4">
+        <div className="fixed bottom-0 right-0 p-4 text-base">
             <p>build on-chain AI with <a href="https://galadriel.com" target="_blank"><img className="h-9 inline align-text-bottom pb-1" src="/logo.png" /></a>
             </p>
         </div>
