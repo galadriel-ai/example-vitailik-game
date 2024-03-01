68de2467851dca494309822161ff93980f7d9303
taivop <taivo@pungas.ee>
2024-02-29 18:22:44 -0700

    logo as img

--- src/components/buildwithgaladriel.tsx
+++ src/components/buildwithgaladriel.tsx
@@ -1,10 +1,7 @@
 export const BuildWithGaladriel = () => (
     <>
         <div className="fixed bottom-0 right-0 p-4">
-            <p>build on-chain AI with
-                <span className="font-PPNeueBit ml-1 text-5xl">
-                    Galadriel
-                </span>
+            <p>build on-chain AI with <img className="h-9 inline align-text-bottom pb-1" src="/logo.png" />
             </p>
         </div>
     </>
