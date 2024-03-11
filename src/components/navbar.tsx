7d1e0219ebb1d595b5708197eeb81f3f88f12749
kristjanpeterson <kristjan@nftport.xyz>
2024-03-11 16:03:04 +0200

    Remove scoreboard

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,7 +7,6 @@ export const Navbar = () => {
       <div className="max-w-6xl mx-auto px-4">
         <div className="flex justify-end">
           <Link href="/" className="px-3 py-2 rounded-md font-medium hover:bg-gray-700">home</Link>
-          {/*<Link href="/scoreboard" className="px-3 py-2 rounded-md font-medium hover:bg-gray-700">scoreboard</Link>*/}
           <Link href="https://galadriel.com" target='_blank' className="px-3 py-2 rounded-md font-medium hover:bg-gray-700">about</Link>
           <Link href="https://twitter.com/Galadriel_AI" className="px-3 py-2 rounded-md font-medium hover:bg-gray-700">x</Link>
         </div>
