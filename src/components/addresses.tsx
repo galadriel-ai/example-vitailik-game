b6fd2018247dda44907cca4a66f152b87c773297
kristjanpeterson <kristjan@nftport.xyz>
2024-03-12 11:31:29 +0200

    Add explorer links

--- src/components/addresses.tsx
+++ src/components/addresses.tsx
@@ -2,19 +2,19 @@ export const Addresses = () => {
   return (
     <div className="text-left text-sm w-full pb-12 lg:pb-0">
       <div>
-        <div className="hidden lg:inline">AI contract: {process.env.NEXT_PUBLIC_CONTRACT_ADDRESS}</div>
+        <div className="hidden lg:inline">AI contract: <a className="underline" href={`https://galadriel.tryethernal.com/address/${process.env.NEXT_PUBLIC_CONTRACT_ADDRESS}?tab=transactions`} target="_blank">{process.env.NEXT_PUBLIC_CONTRACT_ADDRESS}</a></div>
         <div className="inline lg:hidden">AI
-          contract: {(process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "").slice(0, 10)}...
+          contract: <a className="underline" href={`https://galadriel.tryethernal.com/address/${process.env.NEXT_PUBLIC_CONTRACT_ADDRESS}?tab=transactions`} target="_blank">{(process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "").slice(0, 10)}...</a>
         </div>
       </div>
       <div className="pt-4">
         <div className="hidden lg:inline">
           Oracle contract:&nbsp;
-          {process.env.NEXT_PUBLIC_ORACLE_CONTRACT_ADDRESS}
+          <a className="underline" href={`https://galadriel.tryethernal.com/address/${process.env.NEXT_PUBLIC_ORACLE_CONTRACT_ADDRESS}?tab=transactions`} target="_blank">{process.env.NEXT_PUBLIC_ORACLE_CONTRACT_ADDRESS}</a>
         </div>
         <div className="inline lg:hidden">
           Oracle contract:&nbsp;
-          {(process.env.NEXT_PUBLIC_ORACLE_CONTRACT_ADDRESS || "").slice(0, 10)}...
+          <a className="underline" href={`https://galadriel.tryethernal.com/address/${process.env.NEXT_PUBLIC_ORACLE_CONTRACT_ADDRESS}?tab=transactions`} target="_blank">{(process.env.NEXT_PUBLIC_ORACLE_CONTRACT_ADDRESS || "").slice(0, 10)}...</a>
         </div>
       </div>
     </div>
